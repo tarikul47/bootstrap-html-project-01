2afabeb73513144ee17afbb49fa4cc791bc1c997
Tarikul <tarikul47@gmail.com>
2025-10-10 00:29:32 +0600

    Brand Section Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
 (function ($) {
-    "use strict";
-   
+  "use strict";
+
+  var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".tp-swiper-button-next",
+      prevEl: ".tp-swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  var swiper = new Swiper(".myBrandSwiper", {
+    slidesPerView: 'auto',
+    freedome:true,
+    allowTouchMove: false,
+    spaceBetween: 50,
+    loop: true,
+    speed: 4000,
+    autoplay: {
+      delay: 1,
+      disableOnIteraction: true,
+    },
+  });
 })(jQuery);
